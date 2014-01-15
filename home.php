11f78561639823daca850a47abeec292846ec86d
Chase Replogle <Chase Replogle>
2014-01-15 17:22:51 -0600

    Test

--- home.php
+++ home.php
@@ -73,14 +73,21 @@ $(function() {
 							<li><a href="#"><?php echo $i; ?><?php echo "<i class='fi-$icon'></i>"; ?><?php echo $icon_name; ?></a></li>
 
 						<?php endforeach; ?>
-						<?php print_r($posts); ?>
+						<?php wp_reset_postdata(); ?>
 				</ul>
+
+				<?php $i = 0; ?>
+					<?php foreach($posts as $post_object): ?>
+						<?php $i++; ?>
+					<?php if($i==2) break; ?>
 
 				<div class="marketing-content">
 					<h4><?php echo $post_object->post_title; ?></h4>
 		  			<p><?php echo $post_object->content; ?></p>
 					<p><a href="#" class="secondary_button">Become a Client</a></p>
 				</div>
+
+				<?php endforeach; ?>
 
 				<?php wp_reset_postdata(); ?>
   		</div>
