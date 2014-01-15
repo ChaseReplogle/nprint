16e4a2d13f1be4c0a82c5b3954f5356f96848fd4
Chase Replogle <Chase Replogle>
2014-01-15 12:39:11 -0600

    Update Nav

--- page.php
+++ page.php
@@ -8,29 +8,7 @@
 
 <?php include 'nav-account.php'; ?>
 <?php include 'nav-main.php'; ?>
+<?php include 'nav-page.php'; ?>
 
-<nav class="top-bar" data-topbar>
-
-<ul class="title-area">
-    <li class="name">
-     	<h2><?php the_title(); ?></h2>
-    </li>
-  </ul>
-
-<section class="top-bar-section right">
-    <!-- Right Nav Section -->
-    <ul>
-  	  <?php
-			  if($post->post_parent)
-			  	$children = wp_list_pages("title_li=&child_of=".$post->post_parent."&echo=0");
-			  else
-			  	$children = wp_list_pages("title_li=&child_of=".$post->ID."&echo=0");
-			  if ($children) { ?>
-				  <ul>
-				  	<?php echo $children; ?>
-				  </ul>
-		<?php } ?>
-    </ul>
-</nav>	
 
 <?php get_footer(); ?>
