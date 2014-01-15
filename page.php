c5f78480e58bc25cfc85e2c28cd78e7be31cdc02
Chase Replogle <Chase Replogle>
2014-01-15 12:29:26 -0600

    Update Nav

--- page.php
+++ page.php
@@ -9,20 +9,24 @@
 <?php include 'nav-account.php'; ?>
 <?php include 'nav-main.php'; ?>	
 
-<div class="page-header">
-	<h2><?php the_title(); ?></h2>
-	<dl class="sub-nav">
-		<?php
-			  if($post->post_parent)
-			  $children = wp_list_pages("title_li=&child_of=".$post->post_parent."&echo=0");
-			  else
-			  $children = wp_list_pages("title_li=&child_of=".$post->ID."&echo=0");
-			  if ($children) { ?>
-			  <ul>
-			  <?php echo $children; ?>
-			  </ul>
-		<?php } ?>
-	</dl>
+<div class="page-header row">
+	<div class="columns medium-3">
+		<h2><?php the_title(); ?></h2>
+	</div>
+	<div class="columns medium-9">
+		<dl class="sub-nav">
+			<?php
+				  if($post->post_parent)
+				  	$children = wp_list_pages("title_li=&child_of=".$post->post_parent."&echo=0");
+				  else
+				  	$children = wp_list_pages("title_li=&child_of=".$post->ID."&echo=0");
+				  if ($children) { ?>
+					  <ul>
+					  	<?php echo $children; ?>
+					  </ul>
+			<?php } ?>
+		</dl>
+	</div>
 </div>
 
 <?php get_footer(); ?>
