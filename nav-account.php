d9f1389335d19f70534049f82e94031b5da22c71
Chase Replogle <Chase Replogle>
2014-01-15 09:49:06 -0600

    Update Nav

--- nav-account.php
+++ nav-account.php
@@ -1,4 +1,11 @@
 <nav class="top-bar account-nav" data-topbar>
+<ul class="title-area">
+    <li class="name">
+      <h1><a href="#">My Site</a></h1>
+    </li>
+    <li class="toggle-topbar menu-icon"><a href="#">Menu</a></li>
+  </ul>
+  
 <section class="top-bar-section right">
     <!-- Right Nav Section -->
     <ul>
