4f08d14a2b0bc1ba6ee028213918786527c384b9
Chase Replogle <Chase Replogle>
2014-01-15 12:26:09 -0600

    Update Nav

--- page.php
+++ page.php
@@ -9,4 +9,20 @@
 <?php include 'nav-account.php'; ?>
 <?php include 'nav-main.php'; ?>	
 
+<div class="page-header">
+	<h2><?php the_title(); ?></h2>
+	<dl class="sub-nav">
+		<?php
+			  if($post->post_parent)
+			  $children = wp_list_pages("title_li=&child_of=".$post->post_parent."&echo=0");
+			  else
+			  $children = wp_list_pages("title_li=&child_of=".$post->ID."&echo=0");
+			  if ($children) { ?>
+			  <ul>
+			  <?php echo $children; ?>
+			  </ul>
+		<?php } ?>
+	</dl>
+</div>
+
 <?php get_footer(); ?>
