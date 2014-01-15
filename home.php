f6f5ed2f2eea4ab06c69aa3a9f3defdf71faec11
Chase Replogle <Chase Replogle>
2014-01-14 21:39:05 -0600

    Update Columns

--- home.php
+++ home.php
@@ -124,7 +124,7 @@ $(function() {
 	
 	<div class="row nav-row">
 		<div class="columns right medium-10">
-			<ul class="icon-nav large-block-grid-8">
+			<ul class="icon-nav large-block-grid-9">
   				
   				<?php $post_objects = get_field('portfolio_categories');
 					if( $post_objects ): ?>
