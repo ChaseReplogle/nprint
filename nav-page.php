d0ae65dd30bc4acbd96129f49433d47ddc894284
Chase Replogle <Chase Replogle>
2014-01-15 13:12:02 -0600

    Update Nav

--- nav-page.php
+++ nav-page.php
@@ -13,12 +13,10 @@
   	  <?php
 			  if($post->post_parent)
 			  	$children = wp_list_pages("title_li=&child_of=".$post->post_parent."&echo=0");
-			  	
 			  else
 			  	$children = wp_list_pages("title_li=&child_of=".$post->ID."&echo=0");
 			  if ($children) { ?>
 				  <ul>
-					
 				  	<?php echo $children; ?>
 				  </ul>
 		<?php } ?>
