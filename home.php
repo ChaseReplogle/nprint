8fe0b7c6be43bb4aa840f6364fd582a191b7bf02
Chase Replogle <Chase Replogle>
2014-01-15 17:25:14 -0600

    Test

--- home.php
+++ home.php
@@ -83,7 +83,7 @@ $(function() {
 
 				<div class="marketing-content">
 					<h4><?php echo $post_object->post_title; ?></h4>
-		  			<p><?php echo get_field('marketing_icon', $post_object->ID ); ?></p>
+		  			<p><?php echo get_field('marketing_content', $post_object->ID ); ?></p>
 					<p><a href="<?php echo get_field('marketing_link_location', $post_object->ID ); ?>" class="secondary_button"><?php echo get_field('marketing_link_text', $post_object->ID ); ?></a></p>
 				</div>
 
