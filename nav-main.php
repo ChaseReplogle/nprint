d50803d6d8c678996710caec1687290d226ea77b
Chase Replogle <Chase Replogle>
2014-01-16 17:31:53 -0600

    Test

--- nav-main.php
+++ nav-main.php
@@ -23,9 +23,10 @@
 	  	  <li><a href="#">Portfolio</a></li>
 	  	  <li><a href="#">Support</a></li>
 	  	  <li><a href="#">Contact</a></li> -->
-        <script>
-          $( ".menu-item-has-children" ).addClass( "has-dropdown" );
-          $( ".menu-item-has-children ul" ).addClass( "dropdown" );
-        </script>
     </ul>
 </nav>
+
+<script>
+  $( ".menu-item-has-children" ).addClass( "has-dropdown" );
+  $( ".menu-item-has-children ul" ).addClass( "dropdown" );
+</script>
