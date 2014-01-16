3ff2b25bb89f69587d0b453d436ecadf91b9d313
Chase Replogle <Chase Replogle>
2014-01-16 10:24:58 -0600

    Page

--- nav-page.php
+++ nav-page.php
@@ -2,15 +2,12 @@
 
 <nav class="top-bar page-nav" data-topbar>
 <ul class="title-area">
-    <li class="name">
-     	<h2> <?php
+     	<?php
 			  if($post->post_parent) //inner page
 			  	$section = wp_list_pages("title_li=&include=".$post->post_parent);
 			  else // top level page
 			  	$section = wp_list_pages("title_li=&include=".$post->ID."&echo=1"); ?>
 				  	<?php echo $section; ?>
-		</h2>
-	</li>
 </ul>
 
 <section class="top-bar-section right">
