7340511c0bd483732bae03b72e86b58bf2ba1c57
Chase Replogle <Chase Replogle>
2014-01-15 23:37:54 -0600

    Style

--- page.php
+++ page.php
@@ -12,10 +12,6 @@
 
 <div class="row content-row">
 	<div class="columns medium-8 page-main-content">
-		<h1>Test of Page</h1>
-		<hr>
-		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque sed ipsum malesuada, eleifend massa quis, tincidunt nulla. Etiam porta velit non tincidunt tempor. Etiam porta velit non tincidunt tempor.</p>
-		
 		<ul class="example-orbit" data-orbit>
 		  <li>
 		    <img src="http://nprintgraphix.wpengine.com/wp-content/uploads/2014/01/Screen-Shot-2014-01-15-at-12.20.28-PM.png" alt="slide 1" />
@@ -36,6 +32,9 @@
 		    </div>
 		  </li>
 		</ul>
+		<h1>Test of Page</h1>
+		<hr>
+		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque sed ipsum malesuada, eleifend massa quis, tincidunt nulla. Etiam porta velit non tincidunt tempor. Etiam porta velit non tincidunt tempor.</p>
 
 		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque sed ipsum malesuada, eleifend massa quis, tincidunt nulla. Etiam porta velit non tincidunt tempor. Etiam porta velit non tincidunt tempor.</p>
 		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque sed ipsum malesuada, eleifend massa quis, tincidunt nulla. Etiam porta velit non tincidunt tempor. Etiam porta velit non tincidunt tempor.</p>
