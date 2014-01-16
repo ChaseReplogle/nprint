96d372f8b7a459267f44fb41e7fc739daaf9a38b
Chase Replogle <Chase Replogle>
2014-01-16 10:18:27 -0600

    Page

--- nav-page.php
+++ nav-page.php
@@ -3,17 +3,15 @@
 
 <ul class="title-area">
     <li class="name">
-     	<h2><?php
+     	<h2> <?php
 			  if($post->post_parent) //inner page
 			  	$section = wp_list_pages("title_li=&include=".$post->post_parent);
 			  else // top level page
-			  	$section = wp_list_pages("title_li=&child_of=".$post->ID."&echo=1");?>
-				  <ul class="sub-nav-section">
+			  	$section = wp_list_pages("title_li=&child_of=".$post->ID."&echo=1"); ?>
 				  	<?php echo $section; ?>
-				  </ul>
- </h2>
-    </li>
-  </ul>
+		</h2>
+	</li>
+</ul>
 
 <section class="top-bar-section right">
     <!-- Right Nav Section -->
