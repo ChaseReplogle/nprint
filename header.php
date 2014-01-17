d1a89e3ecffdabb4b45bc207b60ad1e88bead70e
Chase Replogle <Chase Replogle>
2014-01-17 00:48:59 -0600

    Style

--- header.php
+++ header.php
@@ -73,7 +73,8 @@
 	<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/_/inc/fonts/foundation-icons.css" />
 	
 	<link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300,600,700,800' rel='stylesheet' type='text/css'>
-	
+	<link href='http://fonts.googleapis.com/css?family=Lato:300,400,700,900' rel='stylesheet' type='text/css'>
+
 	<!-- This is an un-minified, complete version of Modernizr. 
 		 Before you move to production, you should generate a custom build that only has the detects you need. -->
 	<script src="<?php echo get_template_directory_uri(); ?>/_/js/modernizr-2.6.2.dev.js"></script>
