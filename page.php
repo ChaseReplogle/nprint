7a8172c5a0a2dac0e5d25316f751e5461d53f5fd
Chase Replogle <Chase Replogle>
2014-01-16 18:24:16 -0600

    Page

--- page.php
+++ page.php
@@ -97,6 +97,12 @@
 			<a href="#" class="primary_button">Link to Page</a>
 		</div>
 
+		<div class="ad banner">
+			<h3>Title for an Ad Section</h3>
+			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque sed ipsum malesuada, eleifend massa quis, tincidunt nulla. Etiam porta velit non tincidunt tempor. Etiam porta velit non tincidunt tempor.</p>
+			<a href="#" class="primary_button">Link to Page</a>
+		</div>
+
 	</div>
 </div>
 
