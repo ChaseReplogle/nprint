e017e40bb815ca96a6be4e28377652c3eb3e9898
Chase Replogle <Chase Replogle>
2014-01-16 18:16:18 -0600

    Hover

--- page.php
+++ page.php
@@ -104,10 +104,10 @@
 <div class="case-study">
 	<div class="row">
 		<div class="columns medium-9 medium-centered">
-		<h5>Case Study</h5>
-		<h2>Sherman Williams Banners</h2>
-		<p>Lorem ipsum dolor sit amet ipsum malesuada.</p>
-		<a href="#"><img src="http://nprintgraphix.wpengine.com/wp-content/uploads/2014/01/Screen-Shot-2014-01-16-at-4.26.50-PM.png" /></a>
+			<h5>Case Study</h5>
+			<h2>Sherman Williams Banners</h2>
+			<p>Lorem ipsum dolor sit amet ipsum malesuada.</p>
+			<a href="#" class="image-hover"><img src="http://nprintgraphix.wpengine.com/wp-content/uploads/2014/01/Screen-Shot-2014-01-16-at-4.26.50-PM.png" /></a>
 		</div>
 	</div>
 </div>
