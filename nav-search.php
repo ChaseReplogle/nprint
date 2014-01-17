a919d01d76f2b35de7ee085ba0ed0daa55854112
Chase Replogle <Chase Replogle>
2014-01-17 14:34:52 -0600

    Search Form

--- nav-search.php
+++ nav-search.php
@@ -1,3 +1,9 @@
 <aside class="right-off-canvas-menu">
         <a class="exit-off-canvas">Close</a>
+        <hr>
+        <form method="get" id="searchform" action="<?php bloginfo('home'); ?>/">
+			<div><input type="text" size="18" value="<?php echo wp_specialchars($s, 1); ?>" name="s" id="s" />
+				<input type="submit" id="searchsubmit" value="Search" class="btn" />
+			</div>
+		</form>
 </aside>
