6f11622a91bbda42bc1a3ec5c805c509fd5e4e66
Chase Replogle <Chase Replogle>
2014-01-16 19:24:33 -0600

    Page

--- page.php
+++ page.php
@@ -91,18 +91,19 @@
 	</div>
 
 	<div class="columns medium-4 page-sidebar">
+
+		<div class="ad banner" style="background-image: url('http://nprintgraphix.wpengine.com/wp-content/uploads/2014/01/Screen-Shot-2014-01-16-at-6.28.47-PM.png');">
+			<h3>Title for an Ad Section</h3>
+			<img src="http://nprintgraphix.wpengine.com/wp-content/uploads/2014/01/printer.png" />
+			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque sed ipsum malesuada.</p>
+			<a href="#" class="secondary_button">Link to Page</a>
+		</div>
+
 		<div class="ad">
 			<h3>Title for an Ad Section</h3>
 			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque sed ipsum malesuada, eleifend massa quis, tincidunt nulla. Etiam porta velit non tincidunt tempor. Etiam porta velit non tincidunt tempor.</p>
 			<a href="#" class="primary_button">Link to Page</a>
 		</div>
-
-		<div class="ad banner" style="background-image: url('http://nprintgraphix.wpengine.com/wp-content/uploads/2014/01/Screen-Shot-2014-01-16-at-6.28.47-PM.png');">
-			<h3>Title for an Ad Section</h3>
-			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque sed ipsum malesuada, eleifend massa quis, tincidunt nulla. Etiam porta velit non tincidunt tempor. Etiam porta velit non tincidunt tempor.</p>
-			<a href="#" class="secondary_button">Link to Page</a>
-		</div>
-
 	</div>
 </div>
 
