f6e73b5ac073cd04e05b5abd845b3ab4759fa9d9
Chase Replogle <Chase Replogle>
2014-01-19 14:21:48 -0600

    Products

--- archive-products.php
+++ archive-products.php
@@ -17,7 +17,6 @@
 	</div>
 
 	<div class="columns medium-3 product-column">
-		<h4>Products</h4>
 		<ul>
 			<li><a href="#">Banners<i class="fa fa-angle-right"></i></a></li>
 			<li><a href="#">Wraps<i class="fa fa-angle-right"></i></a></li>
@@ -26,7 +25,6 @@
 			<li><a href="#">Vehicles<i class="fa fa-angle-right"></i></a></li>
 			<li><a href="#">Buildings<i class="fa fa-angle-right"></i></a></li>
 		</ul>
-		<h4>Services</h4>
 		<ul>
 			<li><a href="#">Graphic Design<i class="fa fa-angle-right"></i></a></li>
 			<li><a href="#">Proofing<i class="fa fa-angle-right"></i></a></li>
