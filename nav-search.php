602e0846860428eac1984f100ac0313567612e14
Chase Replogle <Chase Replogle>
2014-01-18 21:22:17 -0600

    Search

--- nav-search.php
+++ nav-search.php
@@ -2,7 +2,7 @@
         <div class="close"><a class="exit-off-canvas"><i class="fa fa-minus-square-o"></i></a></div>
         <form method="get" id="searchform" action="<?php bloginfo('home'); ?>/">
 			<div class="search-form"><input type="text" size="18" value="<?php echo wp_specialchars($s, 1); ?>" name="s" id="s" placeholder="Search"/>
-				<button type="submit" id="searchsubmit" class="btn btn-success">
+				<button type="" id="searchsubmit" class="btn btn-success">
                 	<i class="fa fa-search"></i>
             	</button>
 			</div>
