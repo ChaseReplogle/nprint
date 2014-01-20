de606e84429125fcaba128aac3270bc8c9581afb
Chase Replogle <Chase Replogle>
2014-01-19 21:15:26 -0600

    Product

--- single-products.php
+++ single-products.php
@@ -37,8 +37,8 @@
 	  	
 		<div class="row product-item">
 			<div class="columns medium-4 product-photo">
-				<img source="http://nprintgraphix.wpengine.com/wp-content/uploads/2014/01/Screen-Shot-2014-01-14-at-10.01.30-PM.png" />
-				<ul class="clearing-thumbs small-block-grid-5" data-clearing>
+				<img src="http://nprintgraphix.wpengine.com/wp-content/uploads/2014/01/Screen-Shot-2014-01-14-at-10.01.30-PM.png" />
+				<ul class="clearing-thumbs small-block-grid-4" data-clearing>
   					<li><a href="http://nprintgraphix.wpengine.com/wp-content/uploads/2014/01/Screen-Shot-2014-01-14-at-10.01.30-PM.png"><img src="http://nprintgraphix.wpengine.com/wp-content/uploads/2014/01/Screen-Shot-2014-01-14-at-10.01.30-PM.png"></a></li>
 
   					<li><a href="http://nprintgraphix.wpengine.com/wp-content/uploads/2014/01/Screen-Shot-2014-01-14-at-10.01.30-PM.png"><img src="http://nprintgraphix.wpengine.com/wp-content/uploads/2014/01/Screen-Shot-2014-01-14-at-10.01.30-PM.png"></a></li>
