3e413f17e33e4d8aefb4830f49e48b207cea6a57
Chase Replogle <Chase Replogle>
2014-01-21 21:49:15 -0600

    Support

--- archive-support.php
+++ archive-support.php
@@ -16,7 +16,55 @@
 		<hr>
 	</div>
 
-	
+	<div class="team">
+		<div class="row">
+			<div class="columns large-11 large-centered">
+				<h3>We've built a team to help execute and supper your ideas.</h3>
+			</div>
+			<div class="columns large-7 large-centered">
+				<p>Online, on the phone, or in person, our team of experts can help move your project from idea to finished perfection.</p>
+			</div>
+			<div class="columns large-11 large-centered">
+				<img src="<?php echo get_template_directory_uri(); ?>/_/inc/images/team.png" alt="nPrint Team"/>
+			</div>
+		</div>
+	</div>
+
+	<div class="row">
+
+		<div class="columns medium-4">
+			<h3>Products</h3>	
+				<ul>
+					<li><a href="#">Banners</a></li>
+					<li><a href="#">Wraps</a></li>
+					<li><a href="#">Vehicles</a></li>
+					<li><a href="#">Exhibits</a></li>
+					<li><a href="#">Decals</a></li>
+				</ul>
+		</div>
+
+		<div class="columns medium-4">
+			<h3>Art</h3>
+			<ul>
+					<li><a href="#">What is the best file format?</a></li>
+					<li><a href="#">How do I upload files?</a></li>
+					<li><a href="#">How should my files be set up?</a></li>
+					<li><a href="#">What is bleed?</a></li>
+					<li><a href="#">What is the best resolution for large banners?</a></li>
+				</ul>
+		</div>
+
+		<div class="columns medium-4">
+			<h3>Payment and Orders</h3>
+			<ul>
+					<li><a href="#">How can I pay for my purchase?</a></li>
+					<li><a href="#">What is the typical turn around?</a></li>
+					<li><a href="#">Can I order in person?</a></li>
+					<li><a href="#">Can you come on site?</a></li>
+				</ul>
+		</div>
+
+	</div>
 
 
 </div>
