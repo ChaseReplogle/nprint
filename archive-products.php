896e86dbbd29732b24135baa5637eaa18d37735b
Chase Replogle <Chase Replogle>
2014-01-26 22:19:59 -0600

    Nav

--- archive-products.php
+++ archive-products.php
@@ -18,12 +18,7 @@
 
 	<div class="columns medium-3 product-column">
 		<ul>
-			<li><a href="#">Banners<i class="fa fa-angle-right"></i></a></li>
-			<li><a href="#">Wraps<i class="fa fa-angle-right"></i></a></li>
-			<li><a href="#">Die Cust<i class="fa fa-angle-right"></i></a></li>
-			<li><a href="#">Exhibits<i class="fa fa-angle-right"></i></a></li>
-			<li><a href="#">Vehicles<i class="fa fa-angle-right"></i></a></li>
-			<li><a href="#">Buildings<i class="fa fa-angle-right"></i></a></li>
+			<?php wp_list_categories('orderby=name&taxonomy=products'); ?> 
 		</ul>
 		<ul>
 			<li><a href="#">Graphic Design<i class="fa fa-angle-right"></i></a></li>
