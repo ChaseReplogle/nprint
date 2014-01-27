7a334322fdc5d49a7617ca0c0636b1e1c90f2d57
Chase Replogle <Chase Replogle>
2014-01-26 22:50:47 -0600

    nav

--- archive-projects.php
+++ archive-projects.php
@@ -17,17 +17,15 @@
 	</div>
 
 	<div class="medium-3 columns product-column">
-		<ul>
-			<?php
-				$taxonomyName = "product_categories";
-				$terms = get_terms($taxonomyName,array('parent' => 0));
-
-				foreach($terms as $term) {
-				   $icon = get_field('category_icon', 'product_categories_'.$term->term_id);
-			 	   echo '<li><a href="'.get_term_link($term->slug,$taxonomyName).'"><i class="fa'.echo $icon;.'"></i>'.$term->name.'<i class="fa fa-angle-right"></i></a></li>';
-				}
-			?>
-		</ul>
+		<?php
+$taxonomyName = "product_categories";
+$terms = get_terms($taxonomyName,array('parent' => 0));
+foreach($terms as $term) {
+	echo '<ul>';
+    echo '<li><a href="'.get_term_link($term->slug,$taxonomyName).'">'.$term->name.'<i class="fa fa-angle-right"></i></a></li>';
+    echo '</ul>';
+}
+?>
 
 		<div class="panel">
 			<i class="fa fa-search-plus"></i> 
