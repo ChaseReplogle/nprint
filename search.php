0a03b310e1be7bb6b1828b7fd357900661cd5261
Chase Replogle <Chase Replogle>
2014-01-28 20:47:42 -0600

    Search

--- search.php
+++ search.php
@@ -1,57 +1,35 @@
 <?php
 /**
+ * The template for displaying Search Results pages
+ *
  * @package WordPress
- * @subpackage HTML5-Reset-WordPress-Theme
- * @since HTML5 Reset 2.0
+ * @subpackage Twenty_Fourteen
+ * @since Twenty Fourteen 1.0
  */
- get_header(); ?>
 
-<?php include 'nav-account.php'; ?>
-<?php include 'nav-main.php'; ?>
-<div class="contain-to-grid page-nav-warpper">
+get_header(); ?>
 
-<nav class="top-bar page-nav" data-topbar>
-<ul class="title-area">
-			<?php
-				if(is_search()) 
-				  echo "<li><a>Search</a></li>";
-			?>
-</ul>
+	<section id="primary" class="content-area">
+		<div id="content" class="site-content" role="main">
 
-<section class="top-bar-section right">
-    <!-- Right Nav Section -->
-    <ul>
-    </ul>
-</section>
-</nav>
-</div>	
+			<?php if ( have_posts() ) : ?>
 
+			<header class="page-header">
+				<h1 class="page-title"><?php printf( __( 'Search Results for: %s', 'twentyfourteen' ), get_search_query() ); ?></h1>
+			</header><!-- .page-header -->
 
-
-<div class="row content-row">
-	<div class="columns medium-12 page-title">
-		<h1>Search Results</h1>
-		<hr>
-	</div>
-
-
-	<div class="columns medium-12 page-main-content">
-		
-		<?php
+				<?php
 					// Start the Loop.
-					while ( have_posts() ) : the_post(); ?>
+					while ( have_posts() ) : the_post();
 
-						<div class="search-item">
-							<h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-							<h3><a href="<?php the_permalink(); ?>"><?php the_field('full_title'); ?></a></h3>
-							<p><a href="<?php the_permalink(); ?>"><?php the_permalink(); ?></a></p>
-							<p><?php the_excerpt(); ?></p>
-							<a class="support_button"><?php echo get_post_type( get_the_ID() ); ?></a>
-						</div>
+						/*
+						 * Include the post format-specific template for the content. If you want to
+						 * use this in a child theme, then include a file called called content-___.php
+						 * (where ___ is the post format) and that will be used instead.
+						 */
+						get_template_part( 'content', get_post_format() );
 
-						<hr>
-
-					<?php endwhile;
+					endwhile;
 					// Previous/next post navigation.
 					twentyfourteen_paging_nav();
 
@@ -59,20 +37,13 @@
 					// If no content, include the "No posts found" template.
 					get_template_part( 'content', 'none' );
 
-				endif; ?>
+				endif;
+			?>
 
-		<hr>
+		</div><!-- #content -->
+	</section><!-- #primary -->
 
-	</div>
-
-</div>
-
-
-
-
-
-
-<div class="footer-wrapper">
-
-
-<?php get_footer(); ?>
+<?php
+get_sidebar( 'content' );
+get_sidebar();
+get_footer();
