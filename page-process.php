a5953d392ae7ce6e85510856c626befae7229d4b
Chase Replogle <Chase Replogle>
2014-01-28 18:36:40 -0600

    Page

--- page-process.php
+++ page-process.php
@@ -18,93 +18,13 @@ Template Name: Process
 
 <div class="row content-row">
 	<div class="columns medium-12 page-title">
-		<h1>1. Becomeing a Member of nPrint</h1>
+		<h1><?php the_field('full_title'); ?></h1>
 		<hr>
 	</div>
 
 	<div class="columns medium-8 page-main-content">
-		<h3>Large Format Printing</h3>
-		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque sed ipsum malesuada, eleifend massa quis, tincidunt nulla. Etiam porta velit non tincidunt tempor. Etiam porta velit non tincidunt tempor. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque sed ipsum malesuada, eleifend massa quis, tincidunt nulla. Etiam porta velit non tincidunt tempor. Etiam porta velit non tincidunt tempor.</p>
-		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque sed ipsum malesuada, eleifend massa quis, tincidunt nulla. Etiam porta velit non tincidunt tempor. Etiam porta velit non tincidunt tempor.</p>
 		
-		<div class="panel">
-		  <h5>This is a regular panel.</h5>
-		  <p>It has an easy to override visual style, and is appropriately subdued.</p>
-		</div>
-
-		<h2>This is a smaller headline</h2>
-
-		<hr>
-
-		<ul class="clearing-thumbs" data-clearing>
-		  <li><a href="path/to/your/img"><img src="path/to/your/img-th"></a></li>
-		  <li><a href="path/to/your/img"><img src="path/to/your/img-th"></a></li>
-		  <li><a href="path/to/your/img"><img src="path/to/your/img-th"></a></li>
-		</ul>
-
-		<hr>
-
-		<table border="0" bordercolor="" style="background-color:" width="100%" cellpadding="" cellspacing="">
-			<tr>
-				<td>Table Cell</td>
-				<td>Table Cell</td>
-				<td>Table Cell</td>
-			</tr>
-			<tr>
-				<td>Table Cell</td>
-				<td>Table Cell</td>
-				<td>Table Cell</td>
-			</tr>
-			<tr>
-				<td>Table Cell</td>
-				<td>Table Cell</td>
-				<td>Table Cell</td>
-			</tr>
-			<tr>
-				<td>Table Cell</td>
-				<td>Table Cell</td>
-				<td>Table Cell</td>
-			</tr>
-			<tr>
-				<td>Table Cell</td>
-				<td>Table Cell</td>
-				<td>Table Cell</td>
-			</tr>
-		</table>
-
-		<dl class="tabs" data-tab>
-		  <dd class="active"><a href="#panel2-1">Tab 1</a></dd>
-		  <dd><a href="#panel2-2">Tab 2</a></dd>
-		  <dd><a href="#panel2-3">Tab 3</a></dd>
-		  <dd><a href="#panel2-4">Tab 4</a></dd>
-		</dl>
-		<div class="tabs-content">
-		  <div class="content active" id="panel2-1">
-		    <p>First panel content goes here...</p>
-		  </div>
-		  <div class="content" id="panel2-2">
-		    <p>Second panel content goes here...</p>
-		  </div>
-		  <div class="content" id="panel2-3">
-		    <p>Third panel content goes here...</p>
-		  </div>
-		  <div class="content" id="panel2-4">
-		    <p>Fourth panel content goes here...</p>
-		  </div>
-		</div>
-
-		<h3>Large Format Printing</h3>
-		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque sed ipsum malesuada, eleifend massa quis, tincidunt nulla. Etiam porta velit non tincidunt tempor. Etiam porta velit non tincidunt tempor. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque sed ipsum malesuada, eleifend massa quis, tincidunt nulla. Etiam porta velit non tincidunt tempor. Etiam porta velit non tincidunt tempor.</p>
-		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque sed ipsum malesuada, eleifend massa quis, tincidunt nulla. Etiam porta velit non tincidunt tempor. Etiam porta velit non tincidunt tempor.</p>
-		<hr>
-		<h3>Large Format Printing</h3>
-		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque sed ipsum malesuada, eleifend massa quis, tincidunt nulla. Etiam porta velit non tincidunt tempor. Etiam porta velit non tincidunt tempor. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque sed ipsum malesuada, eleifend massa quis, tincidunt nulla. Etiam porta velit non tincidunt tempor. Etiam porta velit non tincidunt tempor.</p>
-		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque sed ipsum malesuada, eleifend massa quis, tincidunt nulla. Etiam porta velit non tincidunt tempor. Etiam porta velit non tincidunt tempor.</p>
-		<hr>
-		<h3>Large Format Printing</h3>
-		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque sed ipsum malesuada, eleifend massa quis, tincidunt nulla. Etiam porta velit non tincidunt tempor. Etiam porta velit non tincidunt tempor. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque sed ipsum malesuada, eleifend massa quis, tincidunt nulla. Etiam porta velit non tincidunt tempor. Etiam porta velit non tincidunt tempor.</p>
-		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque sed ipsum malesuada, eleifend massa quis, tincidunt nulla. Etiam porta velit non tincidunt tempor. Etiam porta velit non tincidunt tempor.</p>
-		
+		<?php the_content(); ?>
 
 	</div>
 
