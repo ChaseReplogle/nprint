ed4836a6bc9531323dc0e61026572c57a42c280f
Chase Replogle <Chase Replogle>
2014-01-30 23:07:04 -0600

    Account

--- nav-account.php
+++ nav-account.php
@@ -1,6 +1,21 @@
-<?php if( ! is_user_logged_in()): ?>
+<?php if( is_user_logged_in()): ?>
 
-Test
+<nav class="top-bar account-nav <?php if (! is_home()) { ?> inner-account-nav <?php } ?>" data-topbar>
+    <?php if (! is_home()) { ?>
+    <ul class="title-area">
+        <li class="name">
+          <a href="/"><img src="<?php echo get_template_directory_uri(); ?>/_/inc/images/logo.png" alt="nPrint Graphix" width="386" height="190" /></a>
+        </li>
+      </ul>
+    <?php } ?>
+    <section class="top-bar-section right">
+        <!-- Right Nav Section -->
+        <ul>
+            <li><a class="" href="#">Dashboard</a></li>
+            <li><a href="#">Account</a></li>
+            <li><a class="right-off-canvas-toggle" id="search-link"><i class="fa fa-search"></i> <span>Search</span></a></li>
+        </ul>
+    </nav>
 
 <?php else: ?>
  <nav class="top-bar account-nav <?php if (! is_home()) { ?> inner-account-nav <?php } ?>" data-topbar>
