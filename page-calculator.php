d17f7a7ffc03a59bf9d64cdf5d286108e5fa84d5
Chase Replogle <Chase Replogle>
2014-02-01 22:31:36 -0600

    Calculator

---
+++ page-calculator.php
@@ -0,0 +1,38 @@
+<?php
+/*
+Template Name: Calculator
+*/
+?>
+
+<?php
+/**
+ * @package WordPress
+ * @subpackage HTML5-Reset-WordPress-Theme
+ * @since HTML5 Reset 2.0
+ */
+ get_header(); ?>
+
+<?php include 'nav-account.php'; ?>
+<?php include 'nav-main.php'; ?>
+<?php include 'nav-dashboard.php'; ?>
+
+<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+
+<div class="row content-row">
+
+	<div class="columns medium-12 page-main-content">
+		
+	<?php the_content(); ?>
+
+	</div>
+
+	<hr>
+
+</div>
+
+<?php endwhile; endif; ?>
+
+<div class="footer-wrapper">
+
+
+<?php get_footer(); ?>
