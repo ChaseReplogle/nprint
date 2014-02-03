6a9062ea51da980d04f80fe27eb239840d1f9f1c
Chase Replogle <Chase Replogle>
2014-02-03 10:31:08 -0600

    Cost

--- page-calculator.php
+++ page-calculator.php
@@ -71,7 +71,7 @@ Template Name: Calculator
       var windowTop = $(window).scrollTop(); // returns number 
  
       if (stickyTop < windowTop){
-        $('#cost').css({ position: 'fixed', top: 20, width: '30%'});
+        $('#cost').css({ position: 'fixed', top: 20, width: '28%'});
       }
       else {
         $('#cost').css({ position: 'static', width: 'auto'});
