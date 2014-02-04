67454c5ef30976594da98d7e679f73be85bab4d8
Chase Replogle <Chase Replogle>
2014-02-03 20:27:45 -0600

    Calculator

--- footer.php
+++ footer.php
@@ -74,6 +74,7 @@
 <script src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.10.3/jquery-ui.min.js"></script>
 <script src="<?php echo get_template_directory_uri(); ?>/_/inc/js/vender/video.js"></script>
 <script src="<?php echo get_template_directory_uri(); ?>/_/inc/js/vender/bigvideo.js"></script>
+<script src="<?php echo get_template_directory_uri(); ?>/_/inc/js/vender/calculator.js"></script>
 <!-- this is where we put our custom functions -->
 <script src="<?php bloginfo('template_directory'); ?>/_/js/functions.js"></script>
 
