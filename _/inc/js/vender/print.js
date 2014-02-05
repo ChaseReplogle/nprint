7940a4a888c61e2f95b96865f5264c93a08fdf2c
Chase Replogle <Chase Replogle>
2014-02-04 22:04:04 -0600

    Print

--- _/inc/js/vender/print.js
+++ _/inc/js/vender/print.js
@@ -12,4 +12,8 @@ var vars = [], hash;
 // Will alert the value of parameter a
 if(['style'] > 'print') {
   alert('This is a print style.');
+  $('link[title="main"]').prop('disabled', true);
+}
+else {
+  $('link[title="print"]').prop('disabled', true);
 }
