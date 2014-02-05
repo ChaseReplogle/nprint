482001ba500b57e4d7dc6e0b7f478135c7a45702
Chase Replogle <Chase Replogle>
2014-02-05 08:25:51 -0600

    Print

--- _/inc/js/vender/print-ck.js
+++ _/inc/js/vender/print-ck.js
@@ -1,1 +1,1 @@
-$.urlParam=function(e){var t=(new RegExp("[\\?&]"+e+"=([^&#]*)")).exec(window.location.href);return t===null?null:t[1]||0};$.urlParam("style");var style=$.urlParam("style");if(style==="print"){jQuery("#main-css").remove();jQuery("#calc-js").remove()}else jQuery("#print-css").remove();
+$.urlParam=function(e){var t=(new RegExp("[\\?&]"+e+"=([^&#]*)")).exec(window.location.href);return t===null?null:t[1]||0};$.urlParam("style");var style=$.urlParam("style");style==="print"?jQuery("#main-css").remove():jQuery("#print-css").remove();
