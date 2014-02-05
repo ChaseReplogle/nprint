3ac27fad123f19aa88745ec17fe0a76582975a95
Chase Replogle <Chase Replogle>
2014-02-04 21:55:19 -0600

    Print

---
+++ _/inc/js/vender/print.js
@@ -0,0 +1,13 @@
+var vars = [], hash;
+    var q = document.URL.split('?')[1];
+    if(q !== undefined){
+        q = q.split('&');
+        for(var i = 0; i < q.length; i++){
+            hash = q[i].split('=');
+            vars.push(hash[1]);
+            vars[hash[0]] = hash[1];
+        }
+}
+
+// Will alert the value of parameter a
+alert(vars["a"]);
--- footer.php
+++ footer.php
@@ -75,6 +75,7 @@
 <script src="<?php echo get_template_directory_uri(); ?>/_/inc/js/vender/video.js"></script>
 <script src="<?php echo get_template_directory_uri(); ?>/_/inc/js/vender/bigvideo.js"></script>
 <script src="<?php echo get_template_directory_uri(); ?>/_/inc/js/vender/calculator.js"></script>
+<script src="<?php echo get_template_directory_uri(); ?>/_/inc/js/vender/print.js"></script>
 <!-- this is where we put our custom functions -->
 <script src="<?php bloginfo('template_directory'); ?>/_/js/functions.js"></script>
 
