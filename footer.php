a76de1f56769f94410723593d87de6b32a419ca6
Chase Replogle <Chase Replogle>
2014-02-05 13:47:47 -0600

    Print

--- footer.php
+++ footer.php
@@ -69,6 +69,22 @@
   $(document).foundation();
 </script>
 
+
+
+
+<script src="<?php echo get_template_directory_uri(); ?>/_/inc/js/vender/imagesloaded.js"></script>
+<script src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.10.3/jquery-ui.min.js"></script>
+<script src="<?php echo get_template_directory_uri(); ?>/_/inc/js/vender/video.js"></script>
+<script src="<?php echo get_template_directory_uri(); ?>/_/inc/js/vender/bigvideo.js"></script>
+<script src="<?php echo get_template_directory_uri(); ?>/_/inc/js/vender/calculator.js"></script>
+
+<!-- this is where we put our custom functions -->
+
+<script src="<?php echo get_template_directory_uri(); ?>/_/inc/js/vender/print.js"></script>
+<script src="<?php bloginfo('template_directory'); ?>/_/js/functions.js"></script>
+
+
+<link rel="stylesheet" id="print-css" href="<?php echo get_template_directory_uri(); ?>/_/inc/css/print.css" />
 <script>
 
 	$ = jQuery;
@@ -98,21 +114,6 @@
 		jQuery('.for-print').remove();
 	}
 </script>
-
-
-<script src="<?php echo get_template_directory_uri(); ?>/_/inc/js/vender/imagesloaded.js"></script>
-<script src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.10.3/jquery-ui.min.js"></script>
-<script src="<?php echo get_template_directory_uri(); ?>/_/inc/js/vender/video.js"></script>
-<script src="<?php echo get_template_directory_uri(); ?>/_/inc/js/vender/bigvideo.js"></script>
-<script src="<?php echo get_template_directory_uri(); ?>/_/inc/js/vender/calculator.js"></script>
-
-<!-- this is where we put our custom functions -->
-
-<script src="<?php echo get_template_directory_uri(); ?>/_/inc/js/vender/print.js"></script>
-<script src="<?php bloginfo('template_directory'); ?>/_/js/functions.js"></script>
-
-
-<link rel="stylesheet" id="print-css" href="<?php echo get_template_directory_uri(); ?>/_/inc/css/print.css" />
 
 <!-- Asynchronous google analytics; this is the official snippet.
 	 Replace UA-XXXXXX-XX with your site's ID and uncomment to enable.
