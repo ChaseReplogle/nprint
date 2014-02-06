74139e22be16f56e6031a10cc9c853bc35a0e4c4
Chase Replogle <Chase Replogle>
2014-02-05 18:29:19 -0600

    Slider

--- page-calculator.php
+++ page-calculator.php
@@ -168,7 +168,7 @@ Template Name: Calculator
 
 			var percentVal=$('.saved-percent input').val();
 			$('.percent input').val(percentVal);
-			$('#cost-total output').val(percentVal);
+			$('#cost-total output').text(percentVal);
 
 			$('.percent input').attr('onchange', 'rangevalue.value=value');
 			$('.percent').after('<output class="cost" id="rangevalue">0</output>');
