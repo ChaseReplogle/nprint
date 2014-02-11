e29f9b595e7e50da54db40af5a629f97c7f3aa7d
Chase Replogle <Chase Replogle>
2014-02-10 20:09:31 -0600

    Test

--- woocommerce/single-product.php
+++ woocommerce/single-product.php
@@ -1,117 +1,121 @@
-
 <?php
 /**
- * @package WordPress
- * @subpackage HTML5-Reset-WordPress-Theme
- * @since HTML5 Reset 2.0
+ * The Template for displaying all single products.
+ *
+ * Override this template by copying it to yourtheme/woocommerce/single-product.php
+ *
+ * @author 		WooThemes
+ * @package 	WooCommerce/Templates
+ * @version     1.6.4
  */
- get_header(); ?>
 
-<?php include '../../nav-account.php'; ?>
-<?php include '../../nav-main.php'; ?>
-<?php include '../../nav-dashboard.php'; ?>
+if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
 
+get_header(); ?>
 
-<div class="row content-row">
+<?php if( is_user_logged_in()): ?>
 
-	<div class="columns medium-8 page-sidebar save-quotes">
-		<div class="company-info">
-			<div class="columns medium-4 company-logo">
-				<img src="http://nprintgraphix.wpengine.com/wp-content/uploads/2014/02/Screen-Shot-2014-02-07-at-10.43.46-AM.png" alt="nPrint Graphix" width="386" height="190" />
-			</div>
+<nav class="top-bar account-nav <?php if (! is_home()) { ?> inner-account-nav <?php } ?>" data-topbar>
+    <?php if (! is_home()) { ?>
+    <ul class="title-area">
+        <li class="name">
+          <a href="/"><img src="<?php echo get_template_directory_uri(); ?>/_/inc/images/logo.png" alt="nPrint Graphix" width="386" height="190" /></a>
+        </li>
+      </ul>
+    <?php } ?>
+    <section class="top-bar-section right">
+        <!-- Right Nav Section -->
+        <ul>
+            <li class="account-image"><a href="/dashboard"><img src="http://nprintgraphix.wpengine.com/wp-content/uploads/2014/02/Screen-Shot-2014-02-07-at-11.37.47-AM.png"></a></li>
+            <li class="account-links"><a href="/account" class="has-dropdown">Adam Deviza</a>
+              <ul class="dropdown">
+                <li><a class="" href="/dashboard">My Quotes</a></li>
+                <?php wp_list_pages('title_li=&child_of=123'); ?>
+              </ul>
+            </li>
+            <li><a class="right-off-canvas-toggle" id="search-link"><i class="fa fa-search"></i> <span>Search</span></a></li>
+        </ul>
+    </nav>
 
-			<div class="columns medium-8 company-contact">
+<?php else: ?>
+ <nav class="top-bar account-nav <?php if (! is_home()) { ?> inner-account-nav <?php } ?>" data-topbar>
+    <?php if (! is_home()) { ?>
+    <ul class="title-area">
+        <li class="name">
+          <a href="/"><img src="<?php echo get_template_directory_uri(); ?>/_/inc/images/logo.png" alt="nPrint Graphix" width="386" height="190" /></a>
+        </li>
+      </ul>
+    <?php } ?>
+    <section class="top-bar-section right">
+        <!-- Right Nav Section -->
+        <ul>
+    	  	  <li><a class="active" href="#">Become a Member</a></li>
+    	  	  <li><a href="#" data-reveal-id="myModal" data-reveal>Log In</a></li>
+    	  	  <li><a class="right-off-canvas-toggle" id="search-link"><i class="fa fa-search"></i> <span>Search</span></a></li>
+        </ul>
+    </nav>
 
-				<h4>Beyond Creative</h4>
-				<p>Adam Deviza</p>
-				<p>2023 S Saratoga Ave</br>
-				Springfirld, MO 65804</p>
-
-				<p>(573) 680-7902</p>
-				<p>adam@breakbeyond.com</p>
-
-				<a href="#" class="small-link">Edit</a>
-			</div>
-			<div class="clear"></div>
-		</div>
-
-	<div class="quotes">
-		<div class="columns medium-12 saved-quotes">
-
-			<h2>Recent Quotes</h2>
-
-			<ul>
-
-			<?php $user_ID = get_current_user_id(); ?> 
-
-			<?php $args = array(
-				'author' 	 => $user_ID,
-				'post_type'  => 'quotes',
-				'orderby' 	 => 'date',
-				'order'      => 'DESC'
-			);
-
-			query_posts( $args ); ?>
-
-			<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-				 <?php $postid = get_the_ID(); ?> 
-				 <?php $calcType = get_field("product"); ?>
-
-				<li>
-					<div class="columns medium-4">
-						<a href="<?php echo site_url(); ?>/calculators/<?php echo $calcType; ?>/?gform_post_id=<?php echo $postid; ?>" class="quote-title"><?php the_title(); ?></a>
-					</div>
-
-					<div class="columns medium-2">
-						<p><?php the_field('client-name'); ?></p>
-					</div>
-
-					<div class="columns medium-6 actions">
-						<span class="quote-actions">
-							<a href="<?php echo site_url(); ?>/calculators/<?php echo $calcType; ?>/?gform_post_id=<?php echo $postid; ?>" class="quote-edit">Edit</a> |
-							<a href="<?php echo site_url(); ?>/calculators/<?php echo $calcType; ?>/?gform_post_id=<?php echo $postid; ?>&style=print" class="quote-print">Print</a> |
-							<a href="#" data-dropdown="delete" class="quote-delete">Delete</a>
-								<div id="delete" class="f-dropdown" data-dropdown-content>
-									<p>You are about to delete this quote. You won't be able to undo this action.</p>
-									<?php if ($post->post_author == $current_user->ID) { ?><p><a href="<?php echo get_delete_post_link( $post->ID ) ?>" class="delete" >Confirm Delete</a></p><?php } ?>
-								</div>
-							<a href="#" class="quote-cart secondary_button">Add to Cart</a>
-						</span>
-					</div>
-				</li>
-
-				
-
-
-			<?php endwhile; endif; ?>
-
-			</ul>	
-
-			<hr>
-
-			<a href="#" class="view-all">View All Quotes</a>	
-		</div>
-	</div>
-</div>
+        <div class="row">
+          <div id="myModal" class="reveal-modal" data-reveal>
+          <a class="close-reveal-modal"><i class="close fa fa-minus-square-o"></i></a>
+          <div class="columns medium-6">
+            <h2>Sign Into Your Account</h2>
+            <?php $args = array(
+              'redirect' => site_url( '/dashboard' ),
+              'label_username' => __( '' ),
+              'label_password' => __( '' ),
+              'remember' => false,
+             ); ?>
+            <?php wp_login_form( $args ); ?>
+            <a href="<?php echo wp_lostpassword_url(); ?>" title="Lost Password">Lost Password?</a>
+            <script>
+              $("#user_login").attr("placeholder", "Username or Email");
+              $("#user_pass").attr("placeholder", "Password");
+            </script>
+          </div>
+          <div class="columns medium-6 membership-wrap right">
+            <div class="membership">
+              <div class="membership-content">
+                <h2>Not Yet a Member?</h2>
+                <p>Lorum ipsum dolor set amour estes lorum dolor.</p>
+                <a href="#" class="support_button">Create an Account</a>
+              </div>
+            </div>
+          </div>
+        </div> 
+    </div>
 
 
-	<div class="columns medium-4 page-main-content">
-		<a href="/calculators">
-			<div class="calculator-button">
-				<i class="fa fa-table"></i>
-				<h3>Start a Quote</h3>
-				<p>Use our online project calculator to create and save quotes.</p>
-				<a href="/calculators" class="secondary_button">Let's Get Started</a>
-			</div>
-		</a>
-	</div>
 
+<?php endif; ?>
+<?php include '/nav-main.php'; ?>
+<?php include '/nav-dashboard.php'; ?>
 
-</div>
+	<?php
+		/**
+		 * woocommerce_before_main_content hook
+		 *
+		 * @hooked woocommerce_output_content_wrapper - 10 (outputs opening divs for the content)
+		 * @hooked woocommerce_breadcrumb - 20
+		 */
+		do_action('woocommerce_before_main_content');
+	?>
 
-<hr>
+		<?php while ( have_posts() ) : the_post(); ?>
+
+			<?php woocommerce_get_template_part( 'content', 'single-product' ); ?>
+
+		<?php endwhile; // end of the loop. ?>
+
+	<?php
+		/**
+		 * woocommerce_after_main_content hook
+		 *
+		 * @hooked woocommerce_output_content_wrapper_end - 10 (outputs closing divs for the content)
+		 */
+		do_action('woocommerce_after_main_content');
+	?>
 
 <div class="footer-wrapper">
-
 
 <?php get_footer(); ?>
