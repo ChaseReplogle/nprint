eea62a16fba9d3d7bbfeacfd2b8cde2cff86b457
Chase Replogle <Chase Replogle>
2014-02-10 20:04:20 -0600

    Wootheme

--- woocommerce/single-product.php
+++ woocommerce/single-product.php
@@ -42,15 +42,6 @@ get_header(); ?>
 		do_action('woocommerce_after_main_content');
 	?>
 
-	<?php
-		/**
-		 * woocommerce_sidebar hook
-		 *
-		 * @hooked woocommerce_get_sidebar - 10
-		 */
-		do_action('woocommerce_sidebar');
-	?>
-
 <div class="footer-wrapper">
 
 <?php get_footer(); ?>
