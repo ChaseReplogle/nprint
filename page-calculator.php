7c30469edfff2657e1ba309db7547db27af800db
Chase Replogle <Chase Replogle>
2014-02-15 14:59:46 -0600

    Dash Nav

--- page-calculator.php
+++ page-calculator.php
@@ -17,8 +17,8 @@ Template Name: Calculator
 <div class="contain-to-grid page-nav-warpper dash-nav-wrapper">
 	<nav class="top-bar page-nav" data-topbar>
 			<li class="columns dash-nav past option"><a href="/dashboard">Dashboard</a></li>
-			<li class="columns dash-nav current"><a href="/dashboard/calculators"><span><i class="fa fa-square-o"></i></span> Calculators</a></li>
-			<li class="columns dash-nav"><a href="#"><span><i class="fa fa-square-o"></i>.</span> Reivew</a></li>
+			<li class="columns dash-nav current"><a href="/dashboard/calculators"><span><i class="fa fa-minus-square-o"></i></span> Calculators</a></li>
+			<li class="columns dash-nav"><a href="#"><span><i class="fa fa-square-o"></i></span> Reivew</a></li>
 			<li class="columns dash-nav option"><a href="/cart"><span><i class="fa fa-square-o"></i></span> Cart</a></li>
 			<li class="columns dash-nav option"><a href="/checkout"><span><i class="fa fa-square-o"></i></span> Checkout</a></li>
 	</nav>
