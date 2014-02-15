6606447291418d2755b4af0bf3cf3ac7c201dc68
Chase Replogle <Chase Replogle>
2014-02-14 22:08:19 -0600

    Dash Nav

--- nav-dashboard.php
+++ nav-dashboard.php
@@ -3,11 +3,11 @@
 	<nav class="top-bar page-nav" data-topbar>
 		<div class="row">
 
-			<li class="columns medium-4"><a href="<?php the_permalink();?>">Dashboard</a></li>
-			<li class="columns medium-2"><a href="<?php the_permalink();?>">1. Calculators</a></li>
-			<li class="columns medium-2"><a href="<?php the_permalink();?>">2. Reivew</a></li>
-			<li class="columns medium-2"><a href="<?php the_permalink();?>">3. Cart</a></li>
-			<li class="columns medium-2"><a href="<?php the_permalink();?>">4. Checkout</a></li>
+			<li class="columns dash-nav"><a href="<?php the_permalink();?>">Dashboard</a></li>
+			<li class="columns dash-nav"><a href="<?php the_permalink();?>">1. Calculators</a></li>
+			<li class="columns dash-nav"><a href="<?php the_permalink();?>">2. Reivew</a></li>
+			<li class="columns dash-nav"><a href="<?php the_permalink();?>">3. Cart</a></li>
+			<li class="columns dash-nav"><a href="<?php the_permalink();?>">4. Checkout</a></li>
 
 			<div class="progress">
 				<span class="meter" style="width: 0%"></span>
