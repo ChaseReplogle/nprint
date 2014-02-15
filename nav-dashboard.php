e420b8f49f1716c2adf58f69b76f35d0f8405722
Chase Replogle <Chase Replogle>
2014-02-15 12:18:16 -0600

    Dash Nav

--- nav-dashboard.php
+++ nav-dashboard.php
@@ -1,13 +1,9 @@
 <div class="contain-to-grid page-nav-warpper dash-nav-wrapper">
-
 	<nav class="top-bar page-nav" data-topbar>
-
 			<li class="columns dash-nav current"><a href="<?php the_permalink();?>">Dashboard</a></li>
 			<li class="columns dash-nav"><a href="<?php the_permalink();?>"><span>1.</span> Calculators</a></li>
 			<li class="columns dash-nav"><a href="<?php the_permalink();?>"><span>2.</span> Reivew</a></li>
 			<li class="columns dash-nav"><a href="<?php the_permalink();?>"><span>3.</span> Cart</a></li>
 			<li class="columns dash-nav"><a href="<?php the_permalink();?>"><span>4.</span> Checkout</a></li>
-
 	</nav>
-
 </div>	
