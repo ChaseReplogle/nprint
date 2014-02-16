2a287b0b1e88b707b6f147960e70cb08edc3d561
Chase Replogle <Chase Replogle>
2014-02-15 19:11:56 -0600

    Cart

--- nav-dashboard.php
+++ nav-dashboard.php
@@ -1,9 +1,9 @@
 <div class="contain-to-grid page-nav-warpper dash-nav-wrapper">
 	<nav class="top-bar page-nav" data-topbar>
 			<li class="columns dash-nav option"><a href="/dashboard">Dashboard</a></li>
-			<li class="columns dash-nav option"><a href="/dashboard/calculators"><span><i class="fa fa-check-square-o"></i></span> Calculators</a></li>
-			<li class="columns dash-nav	disabled"><a href="#"><span><i class="fa fa-check-square-o"></i></span> Reivew</a></li>
-			<li class="columns dash-nav option"><a href="/cart"><span><i class="fa fa-minus-square-o"></i></span> Cart</a></li>
+			<li class="columns dash-nav option"><a href="/dashboard/calculators"><span><i class="fa fa-square-o"></i></span> Calculators</a></li>
+			<li class="columns dash-nav	disabled"><a href="#"><span><i class="fa fa-square-o"></i></span> Reivew</a></li>
+			<li class="columns dash-nav option"><a href="/cart"><span><i class="fa fa-square-o"></i></span> Cart</a></li>
 			<li class="columns dash-nav option"><a href="/checkout"><span><i class="fa fa-square-o"></i></span> Checkout</a></li>
 	</nav>
 </div>
