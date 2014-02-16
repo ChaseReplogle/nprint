784b0e8d8c0657e0199b932dced1cb5bfbd4ca90
Chase Replogle <Chase Replogle>
2014-02-15 19:21:52 -0600

    Dash Nav

--- nav-dashboard.php
+++ nav-dashboard.php
@@ -1,9 +1,9 @@
 <div class="contain-to-grid page-nav-warpper dash-nav-wrapper">
 	<nav class="top-bar page-nav" data-topbar>
 			<li class="columns dash-nav option"><a href="/dashboard">Dashboard</a></li>
-			<li class="columns dash-nav option"><a href="/dashboard/calculators"><span><i class="fa fa-square-o"></i></span> Calculators</a></li>
-			<li class="columns dash-nav	option"><a href="#"><span><i class="fa fa-square-o"></i></span> Reivew</a></li>
-			<li class="columns dash-nav option"><a href="/cart"><span><i class="fa fa-square-o"></i></span> Cart</a></li>
-			<li class="columns dash-nav option"><a href="/checkout"><span><i class="fa fa-square-o"></i></span> Checkout</a></li>
+			<li class="columns dash-nav option"><a href="/dashboard/calculators"><span></span> Calculators</a></li>
+			<li class="columns dash-nav	option"><a href="#"><span></span> Reivew</a></li>
+			<li class="columns dash-nav option"><a href="/cart"><span></span> Cart</a></li>
+			<li class="columns dash-nav option"><a href="/checkout"><span></span> Checkout</a></li>
 	</nav>
 </div>
