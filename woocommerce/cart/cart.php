3ecf9e3c11a2cfd4f3b5d19f78d67ae8ca7f13b0
Chase Replogle <Chase Replogle>
2014-02-15 18:47:51 -0600

    Cart

--- woocommerce/cart/cart.php
+++ woocommerce/cart/cart.php
@@ -9,22 +9,7 @@
 
 if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
 
-get_header(); ?>
 
-<?php include 'wp-content/themes/nprint/nav-account.php'; ?>
-<?php include 'wp-content/themes/nprint/nav-main.php'; ?>
-
-<div class="contain-to-grid page-nav-warpper dash-nav-wrapper">
-	<nav class="top-bar page-nav" data-topbar>
-			<li class="columns dash-nav past option"><a href="/dashboard">Dashboard</a></li>
-			<li class="columns dash-nav past option"><a href="/dashboard/calculators"><span><i class="fa fa-check-square-o"></i></span> Calculators</a></li>
-			<li class="columns dash-nav past option"><a href="#"><span><i class="fa fa-check-square-o"></i></span> Reivew</a></li>
-			<li class="columns dash-nav current"><a href="/cart"><span><i class="fa fa-minus-square-o"></i></span> Cart</a></li>
-			<li class="columns dash-nav disabled"><a href="/checkout"><span><i class="fa fa-square-o"></i></span> Checkout</a></li>
-	</nav>
-</div>	
-
-<?php 
 $woocommerce->show_messages();
 ?>
 
