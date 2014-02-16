8a4681b6025014c8f4e7fa9412dfec15acdcd86b
Chase Replogle <Chase Replogle>
2014-02-15 19:00:04 -0600

    Thank You

--- page-thanks.php
+++ page-thanks.php
@@ -14,8 +14,8 @@ Template Name: Thank You
 <?php include 'nav-account.php'; ?>
 <?php include 'nav-main.php'; ?>
 
-<div class="contain-to-grid page-nav-warpper dash-nav-wrapper">
-	<nav class="top-bar page-nav finished" data-topbar>
+<div class="contain-to-grid page-nav-warpper dash-nav-wrapper finished">
+	<nav class="top-bar page-nav" data-topbar>
 			<li class="columns dash-nav past option"><a href="/dashboard">Dashboard</a></li>
 			<li class="columns dash-nav past option"><a href="/dashboard/calculators"><span><i class="fa fa-check-square-o"></i></span> Calculators</a></li>
 			<li class="columns dash-nav past option"><a href="#"><span><i class="fa fa-check-square-o"></i></span> Reivew</a></li>
