2143d85032f6299c0b7cf85e1a2643d89f482a66
Chase Replogle <Chase Replogle>
2014-02-15 21:31:53 -0600

    Payment

--- woocommerce/checkout/form-checkout.php
+++ woocommerce/checkout/form-checkout.php
@@ -46,17 +46,14 @@ $get_checkout_url = apply_filters( 'woocommerce_get_checkout_url', $woocommerce-
 
 		</div>
 
-	<hr>	
-			<?php do_action( 'woocommerce_checkout_after_customer_details' ); ?>
+		<?php do_action( 'woocommerce_checkout_after_customer_details' ); ?>
 
-			<h3 id="order_review_heading"><?php _e( 'Your order', 'woocommerce' ); ?></h3>
+		<h3 id="order_review_heading"><?php _e( 'Your order', 'woocommerce' ); ?></h3>
 
-		<?php endif; ?>
+	<?php endif; ?>
 
-		<?php do_action( 'woocommerce_checkout_order_review' ); ?>
-
-</div>
+	<?php do_action( 'woocommerce_checkout_order_review' ); ?>
 
 </form>
 
-	<?php do_action( 'woocommerce_after_checkout_form', $checkout ); ?>
+<?php do_action( 'woocommerce_after_checkout_form', $checkout ); ?>
