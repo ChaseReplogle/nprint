8ce0e97225a797cb4c113a9c0618eb9e9541f6fe
Chase Replogle <Chase Replogle>
2014-02-17 15:20:45 -0600

    Calculator

---
+++ page-calculator-select.php
@@ -0,0 +1,47 @@
+<?php
+/*
+Template Name: Calculators
+*/
+?>
+
+<?php
+/**
+ * @package WordPress
+ * @subpackage HTML5-Reset-WordPress-Theme
+ * @since HTML5 Reset 2.0
+ */
+ get_header(); ?>
+
+<?php include 'nav-account.php'; ?>
+<?php include 'nav-main.php'; ?>
+
+<div class="contain-to-grid page-nav-warpper dash-nav-wrapper">
+	<nav class="top-bar page-nav" data-topbar>
+			<li class="columns dash-nav past option"><a href="/dashboard">Dashboard</a></li>
+			<li class="columns dash-nav current"><a href="/dashboard/calculators"><span><i class="fa fa-minus-square-o"></i></span> Calculators</a></li>
+			<li class="columns dash-nav disabled"><a href="#"><span><i class="fa fa-square-o"></i></span> Reivew</a></li>
+			<li class="columns dash-nav disabled"><a href="/cart"><span><i class="fa fa-square-o"></i></span> Cart</a></li>
+			<li class="columns dash-nav disabled"><a href="/checkout"><span><i class="fa fa-square-o"></i></span> Checkout</a></li>
+	</nav>
+</div>
+
+<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+
+<div class="row content-row">
+
+	<div class="columns medium-12 page-main-content">
+		
+	<?php the_content(); ?>
+
+	</div>
+
+	<hr>
+
+</div>
+
+<?php endwhile; endif; ?>
+
+<div class="footer-wrapper">
+
+
+<?php get_footer(); ?>
