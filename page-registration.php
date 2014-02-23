49368a9fd78f5f82b042f6ded28e5817b5d6fc65
Chase Replogle <Chase Replogle>
2014-02-23 14:16:18 -0600

    Registration

--- page-registration.php
+++ page-registration.php
@@ -24,10 +24,6 @@ Template Name: Registration
 <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 
 <div class="row content-row">
-	<div class="columns medium-12 page-title">
-		<h1><?php the_field('full_title'); ?></h1>
-		<hr>
-	</div>
 
 	<div class="columns medium-8 page-main-content">
 		
