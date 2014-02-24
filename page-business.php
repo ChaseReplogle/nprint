988478ec25f4f51f8049adaf6965f082c7e9a44c
Chase Replogle <Chase Replogle>
2014-02-24 11:27:40 -0600

    Business

---
+++ page-business.php
@@ -0,0 +1,58 @@
+<?php
+/*
+Template Name: Business
+*/
+?>
+<?php
+/**
+ * @package WordPress
+ * @subpackage HTML5-Reset-WordPress-Theme
+ * @since HTML5 Reset 2.0
+ */
+ get_header(); ?>
+
+<?php include 'nav-account.php'; ?>
+<?php include 'nav-main.php'; ?>
+<div class="contain-to-grid page-nav-warpper">
+	<nav class="top-bar page-nav" data-topbar>
+		<ul class="title-area">
+			<li><a href="#">Business Information</a></li>
+		</ul>
+	</nav>
+</div>	
+
+<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+
+<div class="row content-row">
+
+	<div class="columns medium-8 page-main-content">
+		
+		<?php the_content(); ?>
+
+	</div>
+
+	<div class="columns medium-4 page-sidebar">
+
+		<h3>How is my business information used?</h3>
+		<ul>
+			<li>Printable Quotes</li>
+			<li>Helps us better understand your needs</li>
+			<li>Support easier</li>
+		</ul>
+	</div>
+</div>
+
+<?php endwhile; endif; ?>
+
+<script>
+	$ = jQuery;
+	$( ".rcp_subscription_message" ).text( "Choose a Business/Organization Type" );	
+</script>
+
+<div class="footer-wrapper">
+
+<div class="row">
+	<hr>
+</div>
+
+<?php get_footer(); ?>
