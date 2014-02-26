9ed1ea22c9d2bd89200bfd6431667130fedf6691
Chase Replogle <Chase Replogle>
2014-02-25 20:40:05 -0600

    Icons

--- page-calculator-select.php
+++ page-calculator-select.php
@@ -47,8 +47,8 @@ Template Name: Calculators
 		<div class=" calc-item">
 			<span>
 			<span class="fa-stack fa-lg">
-				<i class="fa fa-building-o fa-stack-2x"></i>
-				<i class="fa fa-flag fa-stack-1x fa-inverse"></i>
+				<i class="fa fa-circle fa-stack-2x"></i>
+				<i class="fa fa-building-o fa-stack-1x fa-inverse"></i>
 			</span>
 			<div class="clear"></div>
 			<a class="secondary_button" href="/dashboard/calculators/vinyl-banners/">Billboard Wraps</a>
@@ -59,8 +59,8 @@ Template Name: Calculators
 		<div class=" calc-item">
 			<span>
 			<span class="fa-stack fa-lg">
-				<i class="fa fa-pencil-square-o fa-stack-2x"></i>
-				<i class="fa fa-flag fa-stack-1x fa-inverse"></i>
+				<i class="fa fa-circle fa-stack-2x"></i>
+				<i class="fa fa-pencil-square-o fa-stack-1x fa-inverse"></i>
 			</span>
 			<div class="clear"></div>
 			<a class="secondary_button" href="/dashboard/calculators/vinyl-banners/">Adhesive Decals</a>
@@ -71,8 +71,8 @@ Template Name: Calculators
 		<div class=" calc-item">
 			<span>
 			<span class="fa-stack fa-lg">
-				<i class="fa fa-truck fa-stack-2x"></i>
-				<i class="fa fa-flag fa-stack-1x fa-inverse"></i>
+				<i class="fa fa-circle fa-stack-2x"></i>
+				<i class="fa fa-truck fa-stack-1x fa-inverse"></i>
 			</span>
 			<div class="clear"></div>
 			<a class="secondary_button" href="/dashboard/calculators/vinyl-banners/">Vehical Wrap</a>
