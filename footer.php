fa992660d0394fd74703ca4b578a864de369c8bb
Chase Replogle <Chase Replogle>
2014-02-27 10:35:32 -0600

    Footer

--- footer.php
+++ footer.php
@@ -64,14 +64,24 @@
 
 
 <script src="<?php echo get_template_directory_uri(); ?>/_/inc/js/vender/foundation.min.js"></script>
-<script src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.10.3/jquery-ui.min.js"></script>
+
+<script>
+  $(document).foundation();
+</script>
+
+
+
 
 <script src="<?php echo get_template_directory_uri(); ?>/_/inc/js/vender/imagesloaded.js"></script>
+<script src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.10.3/jquery-ui.min.js"></script>
 <script src="<?php echo get_template_directory_uri(); ?>/_/inc/js/vender/video.js"></script>
 <script src="<?php echo get_template_directory_uri(); ?>/_/inc/js/vender/bigvideo.js"></script>
 <script src="<?php echo get_template_directory_uri(); ?>/_/inc/js/vender/calculator.js"></script>
 <script src="<?php echo get_template_directory_uri(); ?>/_/inc/js/vender/sort-table.js"></script>
 
+<!-- this is where we put our custom functions -->
+
+<script src="<?php echo get_template_directory_uri(); ?>/_/inc/js/vender/print.js"></script>
 <script src="<?php bloginfo('template_directory'); ?>/_/js/functions.js"></script>
 
 
