d4aeda27beb770c01149e25b649d13ab2441d4d4
Chase Replogle <Chase Replogle>
2014-02-27 10:33:40 -0600

    Global

--- footer.php
+++ footer.php
@@ -65,7 +65,12 @@
 
 <script src="<?php echo get_template_directory_uri(); ?>/_/inc/js/vender/foundation.min.js"></script>
 <script src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.10.3/jquery-ui.min.js"></script>
-<script src="<?php echo get_template_directory_uri(); ?>/_/js/global-ck.js"></script>
+
+<script src="<?php echo get_template_directory_uri(); ?>/_/inc/js/vender/imagesloaded.js"></script>
+<script src="<?php echo get_template_directory_uri(); ?>/_/inc/js/vender/video.js"></script>
+<script src="<?php echo get_template_directory_uri(); ?>/_/inc/js/vender/bigvideo.js"></script>
+<script src="<?php echo get_template_directory_uri(); ?>/_/inc/js/vender/calculator.js"></script>
+<script src="<?php echo get_template_directory_uri(); ?>/_/inc/js/vender/sort-table.js"></script>
 
 <script src="<?php bloginfo('template_directory'); ?>/_/js/functions.js"></script>
 
