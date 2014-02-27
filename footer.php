5f83e96392c59a7892c0cb038279fa4475a899e1
Chase Replogle <Chase Replogle>
2014-02-27 10:19:54 -0600

    Modernizer

--- footer.php
+++ footer.php
@@ -72,17 +72,6 @@
 
 
 <script src="<?php echo get_template_directory_uri(); ?>/_/js/global.js"></script>
-<script src="<?php echo get_template_directory_uri(); ?>/_/inc/js/vender/imagesloaded.js"></script>
-<script src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.10.3/jquery-ui.min.js"></script>
-<script src="<?php echo get_template_directory_uri(); ?>/_/inc/js/vender/video.js"></script>
-<script src="<?php echo get_template_directory_uri(); ?>/_/inc/js/vender/bigvideo.js"></script>
-<script src="<?php echo get_template_directory_uri(); ?>/_/inc/js/vender/calculator.js"></script>
-<script src="<?php echo get_template_directory_uri(); ?>/_/inc/js/vender/sort-table.js"></script>
-
-<!-- this is where we put our custom functions -->
-
-<script src="<?php echo get_template_directory_uri(); ?>/_/inc/js/vender/print.js"></script>
-<script src="<?php bloginfo('template_directory'); ?>/_/js/functions.js"></script>
 
 
 <link rel="stylesheet" id="print-css" href="<?php echo get_template_directory_uri(); ?>/_/inc/css/print.css" />
