781c08f476818009dae75ed61f4d0df41e1e6d39
Chase Replogle <Chase Replogle>
2014-03-01 11:28:55 -0600

    Account

--- woocommerce/myaccount/my-account.php
+++ woocommerce/myaccount/my-account.php
@@ -14,7 +14,7 @@ global $woocommerce;
 $woocommerce->show_messages(); ?>
 
 <p class="myaccount_user">
-	Hello, <strong><?php echo $current_user->display_name; ?></strong>. From your account dashboard you can view your <a href="/account/orders/">recent orders</a>, manage your <a href="/account/edit-address/">shipping and billing addresses</a>, <a href="/business-information/?gform_post_id=<?php echo $business_id; ?>">user image</a>, <a href="/business-information/?gform_post_id=<?php echo $business_id; ?>">business info</a>, and <a href="/account/change-password/">change your password</a>
+	Hello, <strong><?php echo $current_user->display_name; ?></strong>. From your account dashboard you can view your <a href="/account/orders/">recent orders</a>, manage your <a href="/account/edit-address/">shipping and billing addresses</a>, change your <a href="/business-information/?gform_post_id=<?php echo $business_id; ?>">user image</a>, update <a href="/business-information/?gform_post_id=<?php echo $business_id; ?>">business info</a>, and <a href="/account/change-password/">change your password</a>
 </p>
 
 <?php do_action( 'woocommerce_before_my_account' ); ?>
