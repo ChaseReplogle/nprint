5e2242d38b76e4dfdfc0c0b984a5d7f5f7cb8dca
Chase Replogle <Chase Replogle>
2014-03-02 21:01:51 -0600

    404

--- 404.php
+++ 404.php
@@ -6,8 +6,44 @@
  */
  get_header(); ?>
 
-	<h2><?php _e('Error 404 - Page Not Found','html5reset'); ?></h2>
+<?php include 'nav-account.php'; ?>
+<?php include 'nav-main.php'; ?>
+<?php include 'nav-page.php'; ?>
 
-<?php get_sidebar(); ?>
+<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+
+<div class="row content-row">
+	
+	<div class="columns medium-8">
+		<h2>Sorry, this page doesn't exist</h2>
+		<p>It looks like the page you were looking for dosn't exist. The best thing to do is to start again from the <a href="/">homepage.</a> Feel free to contact us if the problem persists or if you definetly can't find what you're looking for.</p>
+	</div>
+
+	<div class="columns medium-4>">
+		<a href="/support/">Find Support Articles</a>
+		<a href="/contact/">Contact Us</a>
+	</div>
+</div>
+
+<?php endwhile; endif; ?>
+
+
+<div class="footer-wrapper">
+	<div class="g7">
+		<div class="row">
+			<div class="large-centered large-11 columns">
+				<div class="columns medium-5 ">
+					<img class="right" src="<?php echo get_template_directory_uri(); ?>/_/inc/images/g7.jpg" alt="G7"/>
+				</div>
+				
+				<div class="columns medium-7">
+					<h3>G7 Certified Printer</h3>
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque sed ipsum malesuada, eleifend massa quis, tincidunt nulla. Etiam porta velit non tincidunt tempor. </p>
+					<a href="#">Learn More</a>
+				</div>
+			</div>
+		</div>
+	</div>
+
 
 <?php get_footer(); ?>
